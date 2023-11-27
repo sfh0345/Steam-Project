d933d3c991e1b40b7f40f93a0b8734e3bbbf90aa
sfh0345 <sander.holl@student.hu.nl>
2023-11-27 22:07:29 +0100

    vriendenlijst werkt nu volledig. Werkt in gui en werkt als er minder dan 8 vrienden online zijn.

--- getuserfriendlist.py
+++ getuserfriendlist.py
@@ -1,14 +1,9 @@
-import requests
-
-def get_friend_usernames(api_key, steam_id):
+def get_friend_usernames(steamid64):
+    import requests
     # Get the friend list
-    friend_list_url = "https://api.steampowered.com/ISteamUser/GetFriendList/v0001/"
-    params = {
-        'key': api_key,
-        'steamid': steam_id,
-        'relationship': 'all'
-    }
-    response = requests.get(friend_list_url, params=params)
+    friend_list_url = f"https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=B5A67039860C1613632C4795B6C36245&steamid={steamid64}&relationship=all"
+
+    response = requests.get(friend_list_url)
 
     if response.status_code == 200:
         friend_data = response.json()
@@ -18,7 +13,7 @@ def get_friend_usernames(api_key, steam_id):
 
         # Get usernames and online status for all friends at once
         steamids_str = ','.join(map(str, friend_ids))
-        player_summaries_url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={steamids_str}"
+        player_summaries_url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=B5A67039860C1613632C4795B6C36245&steamids={steamids_str}"
         response = requests.get(player_summaries_url)
 
         if response.status_code == 200:
@@ -31,25 +26,23 @@ def get_friend_usernames(api_key, steam_id):
                 if player['personastate'] == 1 or player['personastate'] == 2 or player['personastate'] == 3 or player['personastate'] == 4:
                     online_friends.append(player['personaname'])
 
+            friendsonline = []
             if online_friends:
-                print("Online Friends:")
                 for i, friend in enumerate(online_friends[:8]):
-                    print(f"Friend {i + 1}: {friend}")
-
+                    friendsonline.append(friend)
                 remaining_count = len(online_friends) - 8
-                if remaining_count > 0:
-                    print(f"\nRemaining Online Friends: {remaining_count}")
+                if remaining_count >= 0:
+                    remaining = remaining_count
                 else:
-                    print("\n0 remaining friends online.")
+                    remaining = 0
+                return friendsonline, remaining
             else:
-                print("No online friends.")
+                return 0
         else:
             print(f"Error: {response.status_code}")
     else:
         print(f"Error: {response.status_code}")
 
 # Replace with your API key and the Steam ID of the user
-api_key = "B5A67039860C1613632C4795B6C36245"
-steam_id = "76561197960435530"
 
-get_friend_usernames(api_key, steam_id)
+# get_friend_usernames("76561199022018738")
