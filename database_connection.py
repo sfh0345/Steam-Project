35c77e83a9fa22442deeb89f3f435361f26f491f
Abdl2000 <abdullah.elhannaoui@student.hu.nl>
2024-01-15 16:52:53 +0100

    made some minor changes to the variable names

--- database_connection.py
+++ database_connection.py
@@ -5,9 +5,9 @@ def connect_to_azure_postgresql():
         # Replace the placeholder values with your actual Azure PostgreSQL connection details
         connection_params = {
             "database": "DBsrv",
-            "user": "DBsrv",
-            "password": "your_password",
-            "host": "your_server_name.postgres.database.azure.com",
+            "user": "postgres",
+            "password": "Steam2023!@#",
+            "host": "51.11.163.195",
             "port": 5432,  # Default PostgreSQL port
             "sslmode": "require",
         }
@@ -28,17 +28,4 @@ def close_connection(connection):
             print("Connection closed.")
     except Exception as e:
         print(f"Error: Unable to close the connection. {e}")
-
-
-    except psycopg2.Error as e:
-        print(f"Error: Unable to execute the query. {e}")
         raise
-
-if __name__ == "__main__":
-    azure_postgresql_connection = connect_to_azure_postgresql()
-
-    example_query = "SELECT * FROM your_table_name;"
-    execute_query(azure_postgresql_connection, example_query)
-
-    # Close the connection when done
-    close_connection(azure_postgresql_connection)
