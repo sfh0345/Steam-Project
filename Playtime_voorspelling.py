321fca3c199b662f53c198b4bc07e4c57a5acf0c
Maartenlevi <maarten.eijgenhuijsen@student.hu.nl>
2024-01-21 19:03:34 +0100

    made sure the functions run on the azure database

--- Playtime_voorspelling.py
+++ Playtime_voorspelling.py
@@ -1,19 +1,13 @@
-import psycopg2
-import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime, timedelta
+from database_connection import connect_to_azure_postgresql, close_connection
 
-# Maak een verbinding met de PostgreSQL-database. Staat op localhost!!
 
-conn = psycopg2.connect(
-    database="steam",
-    user="postgres",
-    password="@Hilversum02@",
-    port="5432"
-)
+# Maak een verbinding met de PostgreSQL-database.
+conn = connect_to_azure_postgresql()
 
 # Maak een cursor
 c = conn.cursor()
+
 
 # haal de data op uit de database
 def get_data():
@@ -24,12 +18,12 @@ def get_data():
     return result
 
 
-def gradien_descent(verhouding_rating, average_playtime, learning_rate, Iterations):
-    # verander de de namen naar x en y
+def gradient_descent(verhouding_rating, average_playtime, learning_rate, iterations):
+    # verander de namen naar x en y
     x = verhouding_rating
     y = average_playtime
 
-    # initaliseer de waarden van m en b
+    # initialiseer de waarden van m en b
     m = 0
     b = 0
 
@@ -37,15 +31,15 @@ def gradien_descent(verhouding_rating, average_playtime, learning_rate, Iteratio
     n = float(len(x))
 
     # implementeer gradient descent
-    for i in range(Iterations):
+    for i in range(iterations):
         y_pred = m * x + b
         dm = (-2/n) * sum(x * (y - y_pred))
         db = (-2/n) * sum(y - y_pred)
         m = m - learning_rate * dm
         b = b - learning_rate * db
 
-
     return m, b
+
 
 def linear_regression(verhouding_rating):
     # Haal de data op uit de database
@@ -54,14 +48,14 @@ def linear_regression(verhouding_rating):
     # Zet de data om naar een numpy array
     x = np.array([i[0] for i in data])
     y = np.array([i[1] for i in data])
-    total_reviews = np.array([i[2] for i in data])
 
     # Bereken de m en b waarden
-    m, b = gradien_descent(x, y, 0.0001, 10000)
+    m, b = gradient_descent(x, y, 0.0001, 10000)
 
     # Bereken de y waarden
     y_pred = m * verhouding_rating + b
     return y_pred
+
 
 def voorspel_playtime(gamename):
     # Haal de data op uit de database
@@ -74,11 +68,6 @@ def voorspel_playtime(gamename):
 
     # sluit de database verbinding
     c.close()
-    conn.close()
+    close_connection(conn)
 
-    print(f"De verwachte speeltijd is {y_pred} minuten.")
     return y_pred
-
-
-
-
