e3a76b093657fd8b71d829d2689d9532d16fe5a2
Maartenlevi <maarten.eijgenhuijsen@student.hu.nl>
2024-01-27 11:26:04 +0100

    playtime voorspelling now doesnt have to have an exact match for the inputs

--- Playtime_voorspelling.py
+++ Playtime_voorspelling.py
@@ -11,6 +11,12 @@ c = conn.cursor()
 
 # haal de data op uit de database
 def get_data():
+    """
+       Retrieve game data from the database.
+
+       Returns:
+           list: List of tuples containing (verhouding_rating, average_playtime, total_reviews) for selected games.
+       """
     c.execute("SELECT verhouding_rating, average_playtime, total_reviews "
               "FROM gameproperties "
               "WHERE total_reviews > 8000 AND release_date > '2009-01-01' AND average_playtime < 15000 AND average_playtime > 4000")
@@ -19,6 +25,18 @@ def get_data():
 
 
 def gradient_descent(verhouding_rating, average_playtime, learning_rate, iterations):
+    """
+       Perform gradient descent to find the coefficients (m, b) for linear regression.
+
+       Args:
+           verhouding_rating (numpy.ndarray): Array of verhouding_rating values.
+           average_playtime (numpy.ndarray): Array of average_playtime values.
+           learning_rate (float): Learning rate for gradient descent.
+           iterations (int): Number of iterations for gradient descent.
+
+       Returns:
+           tuple: Coefficients (m, b) for the linear regression model.
+       """
     # verander de namen naar x en y
     x = verhouding_rating
     y = average_playtime
@@ -42,6 +60,15 @@ def gradient_descent(verhouding_rating, average_playtime, learning_rate, iterati
 
 
 def linear_regression(verhouding_rating):
+    """
+       Perform linear regression based on the provided verhouding_rating.
+
+       Args:
+           verhouding_rating (float): Verhouding rating of a specific game.
+
+       Returns:
+           float: Predicted playtime based on linear regression.
+       """
     # Haal de data op uit de database
     data = get_data()
 
@@ -58,12 +85,24 @@ def linear_regression(verhouding_rating):
 
 
 def voorspel_playtime(gamename):
+    """
+       Predict playtime for a given game.
+
+       Args:
+           gamename (str or int): Name or app ID of the game.
+
+       Returns:
+           int or None: Predicted playtime in hours or None if prediction failed.
+
+       Raises:
+           ValueError: If no data is found for the given gamename or appid.
+       """
 
     if isinstance(gamename, str):
         # Haal de data op uit de database
         c.execute("SELECT verhouding_rating "
                   "FROM gameproperties "
-                  "WHERE name = %s", (gamename,))
+                  "WHERE name ILIKE %s", ('%' + gamename + '%',))
     elif isinstance(gamename, int):
         c.execute("SELECT verhouding_rating "
                   "FROM gameproperties "
@@ -72,14 +111,17 @@ def voorspel_playtime(gamename):
         print("Er is iets mis gegaan")
 
     result = c.fetchone()
+
+    if result is None:
+        raise ValueError("No data found for the given gamename or appid")
+
     verhouding_rating = result[0]
     y_pred = linear_regression(verhouding_rating)
     pred_playtime = round(y_pred / 60)
 
     # sluit de database verbinding
-    # c.close()
-    # close_connection(conn)
+    c.close()
+    close_connection(conn)
 
     return pred_playtime
 
-print(voorspel_playtime("Grand Theft Auto V"))
