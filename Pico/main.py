143be2bb38c2024d90967c6fb3830a8f78fa50c1
Abdl2000 <abdullah.elhannaoui@student.hu.nl>
2024-01-29 23:36:29 +0100

    updated the comments and docstring

--- Pico/main.py
+++ Pico/main.py
@@ -4,29 +4,31 @@ from machine import I2C, Pin
 from pico_i2c_lcd import I2cLcd
 
 """
-Dit is de code om de componenten die op de Pico zijn aangesloten uit te schakelen, dit zijn het LCD-scherm en de NeoPixel. 
-Deze code wordt uitgevoerd zodra de gebruiker het dashboard heeft afgesloten.
+This is the code to deactivate the components connected to the Pico, which are the LCD screen and the NeoPixel. 
+This code is executed once the user has closed the dashboard.
 
 """
 
-# Configureer parameters voor het LCD-scherm
+# Configure parameters for the LCD
 i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=400000)
 I2C_ADDR = i2c.scan()[0]
 lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
 
-# Configureer parameters voor de NeoPixels
+# Configure parameters for the NeoPixels
 np = neopixel.NeoPixel(Pin(9), 8)
 
 
-
+# Function that turns off the LCD
 def turn_lcd_off():
-    lcd.backlight_off() # schakel het LCD-scherm uit
+    lcd.backlight_off() # turn off the LCD
 
 
+# Function that turns off the Neopixel
 def turn_neopixel_off():
-    np.fill((0, 0, 0))  # schakel alle pixels uit(black)
-    np.write() # Update de NeoPixel
+    np.fill((0, 0, 0))  # turn of all pixels (black)
+    np.write() # Update the noepixel
 
 
+# Call the functions
 turn_lcd_off()
 turn_neopixel_off()
